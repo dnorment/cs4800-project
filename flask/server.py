b9d5acad89a5f06c9f3c38df632137373d464516
Kevin Moz <kevinmoz26@gmail.com>
2020-04-23 18:43:44 -0700

    Updated server.py

--- flask/server.py
+++ flask/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+import requests
 
 app = Flask(__name__)
 api = Api(app)
@@ -7,6 +8,15 @@ api = Api(app)
 class YeetBot(Resource):
     def get(self):
         return {'yeet': 'yeet'}
+    
+    def post(self):
+        json = request.get_json()
+        #TODO validate json
+        message = json['inputText']
+        clientid = json['clientId']
+        r = requests.post('https://runtime.lex.us-west-2.amazonaws.com/bot/PsychBot/alias/PsychBot/user/'+clientid+'/text', data=message)
+        return {'you get': r.text}, 200
+
 
 api.add_resource(YeetBot, '/')
 
