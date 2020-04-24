2cf5cb92e6230efe0044e51af961d3be26ecc8bd
Kevin Moz <kevinmoz26@gmail.com>
2020-04-23 17:45:22 -0700

    added basic models
    yeeted

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from .models import Hero
+from .models import Bot
+
+
+admin.site.register(Bot)
+admin.site.register(Hero)
